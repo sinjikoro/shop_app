7ae14c65f4d7b989b0331e9635f84619311bd22a
sinjikoro <shinnaga.dev@gmail.com>
2023-01-18 01:44:42 +0900

    Using "Consumer" instead of "Provider.of"

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -9,30 +9,30 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
-
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          leading: IconButton(icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            onPressed: () => product.toggleFavoriteStatus(),
-            color: Theme.of(context).colorScheme.secondary,
-            ),
-          title: Text(product.title, textAlign: TextAlign.center,),
-          trailing: IconButton(
-            icon: const Icon(Icons.shopping_cart),
-            onPressed: () {},
-            color: Theme.of(context).colorScheme.secondary,),
-        ),
-        child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: product.id);
-          },
-          child: Image.network(product.imageUrl, fit: BoxFit.cover,)
+    return Consumer<Product>(
+      builder: (BuildContext context, product, Widget? child) => ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: IconButton(icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              onPressed: () => product.toggleFavoriteStatus(),
+              color: Theme.of(context).colorScheme.secondary,
+              ),
+            title: Text(product.title, textAlign: TextAlign.center,),
+            trailing: IconButton(
+              icon: const Icon(Icons.shopping_cart),
+              onPressed: () {},
+              color: Theme.of(context).colorScheme.secondary,),
           ),
-        ),
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: product.id);
+            },
+            child: Image.network(product.imageUrl, fit: BoxFit.cover,)
+            ),
+          ),
+      ),
     );
   }
 }
