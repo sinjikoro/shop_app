bdca53b23b54bccdc6682131eec278a00d70ae6c
sinjikoro <shinnaga.dev@gmail.com>
2023-01-25 01:45:03 +0900

    217. Snackbars & Undoing "Add to Cart" Actions

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -54,6 +54,25 @@ class Cart with ChangeNotifier {
     notifyListeners();
   }
 
+  removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId]!.quantity > 1) {
+      _items.update(
+        productId,
+        (existingItem) => CartItem(
+            id: existingItem.id,
+            title: existingItem.title,
+            price: existingItem.price,
+            quantity: existingItem.quantity - 1),
+      );
+    } else {
+      _items.remove(productId);
+    }
+    notifyListeners();
+  }
+
   clear() {
     _items = {};
     notifyListeners();
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -13,29 +13,53 @@ class ProductItem extends StatelessWidget {
     final product = Provider.of<Product>(context, listen: false);
     final cart = Provider.of<Cart>(context, listen: false);
 
-    return 
-      ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: GridTile(
-          footer: GridTileBar(
-            backgroundColor: Colors.black87,
-            leading: IconButton(icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
-              onPressed: () => product.toggleFavoriteStatus(),
-              color: Theme.of(context).colorScheme.secondary,
-              ),
-            title: Text(product.title, textAlign: TextAlign.center,),
-            trailing: IconButton(
-              icon: const Icon(Icons.shopping_cart),
-              onPressed: () => cart.addItem(product.id, product.price, product.title),
-              color: Theme.of(context).colorScheme.secondary,),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border),
+            onPressed: () => product.toggleFavoriteStatus(),
+            color: Theme.of(context).colorScheme.secondary,
           ),
-          child: GestureDetector(
-            onTap: () {
-              Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: product.id);
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {
+              cart.addItem(product.id, product.price, product.title);
+              // Scaffold.of(context).showSnackBar(..);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text('Added item to cart!'),
+                  duration: const Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: (() {
+                      cart.removeSingleItem(product.id);
+                    }),
+                  ),
+                ),
+              );
             },
-            child: Image.network(product.imageUrl, fit: BoxFit.cover,)
-            ),
+            color: Theme.of(context).colorScheme.secondary,
           ),
-      );
+        ),
+        child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                  arguments: product.id);
+            },
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            )),
+      ),
+    );
   }
 }
