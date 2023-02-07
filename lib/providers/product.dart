256bb37d38a7e310f40d3151f6998894b435660e
sinjikoro <shinnaga.dev@gmail.com>
2023-02-07 23:59:01 +0900

    256. Updating the Favorite Status Optimistically

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String? id;
@@ -17,8 +20,31 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  toggleFavoriteStatus() {
+  _setFavoriteValue(bool value) {
+    isFavorite = value;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url =
+        'https://shop-app-shinnaga-default-rtdb.firebaseio.com/product/$id.json';
+    try {
+      final response = await http.patch(
+        Uri.parse(url),
+        body: json.encode(
+          {
+            'isFavorite': isFavorite,
+          },
+        ),
+      );
+      if (response.statusCode >= 400) {
+        _setFavoriteValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavoriteValue(oldStatus);
+    }
   }
 }
