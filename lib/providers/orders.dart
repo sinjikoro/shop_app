fb2eca4a0545697e05a5554463bfbac65174a425
sinjikoro <shinnaga.dev@gmail.com>
2023-02-08 00:50:59 +0900

    258. Fetching Orders & Fixing an Issue

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:shop_app/providers/cart.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop_app/providers/product.dart';
 
 class OrderItem {
   final String id;
@@ -19,41 +20,67 @@ class OrderItem {
 }
 
 class Orders extends ChangeNotifier {
-  final List<OrderItem> _orders = [];
+  List<OrderItem> _orders = [];
 
   List<OrderItem> get orders {
     return [..._orders];
   }
 
-  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
-    try {
-      const url =
-          'https://shop-app-shinnaga-default-rtdb.firebaseio.com/orders.json';
-      final timestamp = DateTime.now();
-      http.post(Uri.parse(url),
-          body: json.encode({
-            'amount': total,
-            'dateTime': timestamp.toIso8601String(),
-            'products': cartProducts
-                .map((cp) => {
-                      'id': cp.id,
-                      'title': cp.title,
-                      'quantity': cp.quantity,
-                      'price': cp.price,
-                    })
-                .toList(),
-          }));
+  Future<void> fetchAndSetOrders() async {
+    const url =
+        'https://shop-app-shinnaga-default-rtdb.firebaseio.com/orders.json';
+    final response = await http.get(Uri.parse(url));
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>?;
+    if (extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(OrderItem(
+        id: orderId,
+        amount: orderData['amount'],
+        dateTime: DateTime.parse(orderData['dateTime']),
+        products: (orderData['products'] as List<dynamic>)
+            .map((item) => CartItem(
+                  id: item['id'],
+                  title: item['title'],
+                  quantity: item['quantity'],
+                  price: item['price'],
+                ))
+            .toList(),
+      ));
+    });
+    _orders = loadedOrders;
+    notifyListeners();
+  }
 
-      _orders.insert(
-        0,
-        OrderItem(
-          id: DateTime.now().toString(),
-          amount: total,
-          products: cartProducts,
-          dateTime: DateTime.now(),
-        ),
-      );
-      notifyListeners();
-    } catch (error) {}
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    const url =
+        'https://shop-app-shinnaga-default-rtdb.firebaseio.com/orders.json';
+    final timestamp = DateTime.now();
+    http.post(Uri.parse(url),
+        body: json.encode({
+          'amount': total,
+          'dateTime': timestamp.toIso8601String(),
+          'products': cartProducts
+              .map((cp) => {
+                    'id': cp.id,
+                    'title': cp.title,
+                    'quantity': cp.quantity,
+                    'price': cp.price,
+                  })
+              .toList(),
+        }));
+
+    _orders.insert(
+      0,
+      OrderItem(
+        id: DateTime.now().toString(),
+        amount: total,
+        products: cartProducts,
+        dateTime: DateTime.now(),
+      ),
+    );
+    notifyListeners();
   }
 }
--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -5,10 +5,31 @@ import 'package:shop_app/widgets/app_drawer.dart';
 import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 
-class OrderScreen extends StatelessWidget {
+class OrderScreen extends StatefulWidget {
   static const routeName = '/order';
 
   const OrderScreen({super.key});
+
+  @override
+  State<OrderScreen> createState() => _OrderScreenState();
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+  var _isLoading = false;
+
+  @override
+  void initState() {
+    setState(() {
+      _isLoading = true;
+    });
+    Future.delayed(Duration.zero).then((_) async {
+      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+    });
+    setState(() {
+      _isLoading = false;
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
