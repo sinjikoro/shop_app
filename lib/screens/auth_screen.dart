3639792a1feb0cb72d9435f69d49215ba62dd611
sinjikoro <shinnaga.dev@gmail.com>
2023-02-15 12:21:40 +0900

    283. Using the AnimatedBuilder Widget

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -212,13 +212,17 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        // height: _authMode == AuthMode.signup ? 320 : 260,
-        height: _heightAnimation.value.height,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.signup ? 320 : 260),
-        width: deviceSize.width * 0.75,
-        padding: const EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: ((ctx, ch) => Container(
+              // height: _authMode == AuthMode.signup ? 320 : 260,
+              height: _heightAnimation.value.height,
+              constraints: BoxConstraints(
+                  minHeight: _authMode == AuthMode.signup ? 320 : 260),
+              width: deviceSize.width * 0.75,
+              padding: const EdgeInsets.all(16.0),
+              child: ch,
+            )),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
