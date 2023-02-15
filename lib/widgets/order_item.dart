9d069bcce081ba5f78302cf63a9088f0ff503ad6
sinjikoro <shinnaga.dev@gmail.com>
2023-02-16 01:24:10 +0900

    289. Practice: Animating Order Boxes

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -18,51 +18,61 @@ class _OrderItemState extends State<OrderItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(10),
-      child: Column(children: [
-        ListTile(
-          title: Text('\$${widget.order.amount}'),
-          subtitle: Text(
-              DateFormat('dd MM yyyy hh:mm').format(widget.order.dateTime)),
-          trailing: IconButton(
-            icon: _expanded
-                ? const Icon(Icons.expand_less)
-                : const Icon(Icons.expand_more),
-            onPressed: () {
-              setState(() {
-                _expanded = !_expanded;
-              });
-            },
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height:
+          _expanded ? min(widget.order.products.length * 20 + 110, 200) : 95,
+      child: Card(
+        margin: const EdgeInsets.all(10),
+        child: Column(children: [
+          ListTile(
+            title: Text('\$${widget.order.amount}'),
+            subtitle: Text(
+                DateFormat('dd MM yyyy hh:mm').format(widget.order.dateTime)),
+            trailing: IconButton(
+              icon: _expanded
+                  ? const Icon(Icons.expand_less)
+                  : const Icon(Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
           ),
-        ),
-        if (_expanded)
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-            height: min(widget.order.products.length * 20 + 10, 100),
-            child: ListView(children: [
-              ...widget.order.products
-                  .map((prod) => Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(prod.title,
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 300),
+            height: _expanded
+                ? min(widget.order.products.length * 20 + 10, 100)
+                : 0,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: min(widget.order.products.length * 20 + 10, 100),
+              child: ListView(children: [
+                ...widget.order.products
+                    .map((prod) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(prod.title,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                            Text(
+                              '${prod.quantity}x \$${prod.price}',
                               style: const TextStyle(
                                 fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              )),
-                          Text(
-                            '${prod.quantity}x \$${prod.price}',
-                            style: const TextStyle(
-                              fontSize: 18,
-                              color: Colors.grey,
+                                color: Colors.grey,
+                              ),
                             ),
-                          ),
-                        ],
-                      ))
-                  .toList(),
-            ]),
+                          ],
+                        ))
+                    .toList(),
+              ]),
+            ),
           )
-      ]),
+        ]),
+      ),
     );
   }
 }
